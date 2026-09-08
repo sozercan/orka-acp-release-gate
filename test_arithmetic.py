125c758db40f03cf1cecb5268cee1da6d2b13428
Sertac Ozercan <sozercan@gmail.com>
2026-09-08 16:52:41 -0700

    test: add subtraction regression fixture
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>

--- test_arithmetic.py
+++ test_arithmetic.py
@@ -1,11 +1,14 @@
 import unittest
 
-from arithmetic import add
+from arithmetic import add, subtract
 
 
 class ArithmeticTests(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(7, 4), 3)
 
 
 if __name__ == "__main__":
